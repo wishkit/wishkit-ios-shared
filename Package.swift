0bdb6056859b4aaac933b3f86359687e4753ebce
Martin Lasek <heylasek@gmail.com>
2023-02-10 21:11:13 -0800

    changes

--- Package.swift
+++ Package.swift
@@ -3,18 +3,15 @@ import PackageDescription
 
 let package = Package(
     name: "wishkit-ios-shared",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        .library(
-            name: "WishKitShared",
-            targets: ["WishKitShared"]),
+        .library(name: "WishKitShared", targets: ["WishKitShared"])
     ],
     dependencies: [],
     targets: [
-        .target(
-            name: "WishKitShared",
-            dependencies: []),
-        .testTarget(
-            name: "WishKitSharedTests",
-            dependencies: ["WishKitShared"]),
+        .target(name: "WishKitShared", dependencies: []),
+        .testTarget(name: "WishKitSharedTests", dependencies: ["WishKitShared"])
     ]
 )
