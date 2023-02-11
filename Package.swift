e65181700ecb9fff4ad3213ce8a0cb4173b07cf8
Martin Lasek <heylasek@gmail.com>
2023-02-10 21:17:05 -0800

    remove platform restrictioon

--- Package.swift
+++ Package.swift
@@ -3,9 +3,6 @@ import PackageDescription
 
 let package = Package(
     name: "wishkit-ios-shared",
-    platforms: [
-        .iOS(.v13)
-    ],
     products: [
         .library(name: "WishKitShared", targets: ["WishKitShared"])
     ],
