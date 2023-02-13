28f42ab7725cf483f59605935ba530cf8a4aa4eb
Martin Lasek <heylasek@gmail.com>
2023-02-12 22:26:57 -0800

    wish description is not optional

--- Sources/WishKitShared/Wish/Request/CreateWishRequest.swift
+++ Sources/WishKitShared/Wish/Request/CreateWishRequest.swift
@@ -10,11 +10,11 @@ public struct CreateWishRequest: Codable {
 
     public let title: String
 
-    public var description: String?
+    public var description: String
 
     public var state: WishState
 
-    public init(title: String, description: String? = nil, state: WishState = .pending) {
+    public init(title: String, description: String, state: WishState = .pending) {
         self.title = title
         self.description = description
         self.state = state
