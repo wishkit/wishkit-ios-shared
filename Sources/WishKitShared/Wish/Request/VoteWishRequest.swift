b05ef891775bf85b4ff4420d49bfaf891fdb6f39
Martin Lasek <heylasek@gmail.com>
2023-02-13 18:07:18 -0800

    added VoteRequest

---
+++ Sources/WishKitShared/Wish/Request/VoteWishRequest.swift
@@ -0,0 +1,18 @@
+//
+//  VoteWishRequest.swift
+//  wishkit-ios-shared
+//
+//  Created by Martin Lasek on 2/13/23.
+//  Copyright © 2023 Martin Lasek. All rights reserved.
+//
+
+import Foundation
+
+public struct VoteWishRequest: Codable {
+
+    let wishId: UUID
+
+    public init(wishId: UUID) {
+        self.wishId = wishId
+    }
+}
