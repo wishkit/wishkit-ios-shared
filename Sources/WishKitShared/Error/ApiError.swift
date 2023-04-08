5141028e445287a6ff95554faa7195de1c95f5cd
Martin Lasek <heylasek@gmail.com>
2023-04-08 14:32:37 -0700

    added new error enum

---
+++ Sources/WishKitShared/Error/ApiError.swift
@@ -0,0 +1,39 @@
+//
+//  ApiError.swift
+//  wishkit-ios-shared
+//
+//  Created by Martin Lasek on 4/7/23.
+//  Copyright © 2023 Martin Lasek. All rights reserved.
+//
+
+public struct ApiError: Error, Codable {
+
+    public let reason: Reason
+
+    public init(reason: Reason) {
+        self.reason = reason
+    }
+
+    public enum Reason: String, Codable {
+        case requestResultedInError
+        case wrongBearerToken
+        case unknown
+        case couldNotCreateRequest
+        case couldNotDecodeBackendResponse
+
+        public var description: String {
+            switch self {
+            case .requestResultedInError:
+                return "Backend responded with an error."
+            case .wrongBearerToken:
+                return "Wrong Bearer Token."
+            case .unknown:
+                return "An unknown error occured."
+            case .couldNotCreateRequest:
+                return "Could not create request."
+            case .couldNotDecodeBackendResponse:
+                return "Could not decode backend response."
+            }
+        }
+    }
+}
