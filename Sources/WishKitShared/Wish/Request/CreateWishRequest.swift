118c9c482e4ad57c65d664283516425b98616483
Martin Lasek <heylasek@gmail.com>
2023-08-27 23:40:43 -0700

    added email to create wish request

--- Sources/WishKitShared/Wish/Request/CreateWishRequest.swift
+++ Sources/WishKitShared/Wish/Request/CreateWishRequest.swift
@@ -12,11 +12,19 @@ public struct CreateWishRequest: Codable {
 
     public var description: String
 
+    public var email: String?
+
     public var state: WishState
 
-    public init(title: String, description: String, state: WishState = .pending) {
+    public init(
+        title: String,
+        description: String,
+        email: String? = nil,
+        state: WishState = .pending
+    ) {
         self.title = title
         self.description = description
+        self.email = email
         self.state = state
     }
 }
