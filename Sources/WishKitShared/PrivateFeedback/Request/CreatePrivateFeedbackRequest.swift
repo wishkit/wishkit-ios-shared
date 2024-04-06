634e5d82412cea5970f94fc9990a6e9d357f2d92
Martin Lasek <heylasek@gmail.com>
2024-04-06 10:57:09 -0700

    introduced private feedback DTOs

---
+++ Sources/WishKitShared/PrivateFeedback/Request/CreatePrivateFeedbackRequest.swift
@@ -0,0 +1,19 @@
+//
+//  CreatePrivateFeedbackRequest.swift
+//  WishKitShared
+//
+//  Created by Martin Lasek on 4/6/24.
+//  Copyright © 2024 Martin Lasek. All rights reserved.
+//
+
+public struct CreatePrivateFeedbackRequest: Codable {
+
+    public let email: String?
+
+    public let description: String
+
+    public init(email: String? = nil, description: String) {
+        self.email = email
+        self.description = description
+    }
+}
---
+++ Sources/WishKitShared/PrivateFeedback/Response/CreatePrivateFeedbackResponse.swift
@@ -0,0 +1,19 @@
+//
+//  CreatePrivateFeedbackRequest.swift
+//  WishKitShared
+//
+//  Created by Martin Lasek on 4/6/24.
+//  Copyright © 2024 Martin Lasek. All rights reserved.
+//
+
+public struct CreatePrivateFeedbackResponse: Codable {
+
+    public let email: String?
+
+    public let description: String
+
+    init(email: String? = nil, description: String) {
+        self.email = email
+        self.description = description
+    }
+}
